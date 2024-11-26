41095845625cda19f826d30a552c98dc3bdf1a91
TwisT404 <cedric.tanghe1303@gmail.com>
2024-11-26 18:04:39 +0100

    new fetch API en GET(test get value to print it)

--- app/deck/ChooseDecks.tsx
+++ app/deck/ChooseDecks.tsx
@@ -1,11 +1,68 @@
-import { BackHandler, Text, View } from "react-native";
+import { ActivityIndicator, BackHandler, FlatList, Pressable, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function ChooseDecks() {
+    //Block the return to the first page
     BackHandler.addEventListener("hardwareBackPress", ()=>{
         return true;
-    })
+    });
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    //Fetch on URL API in method GET
+    const url = "https://mdubois.alwaysdata.net/apiReigns/v1/reigns/deck";
+    let nbMax = 1;
+    let nbMin = 10;
+
+    const getAllDecksFromAPI = async () => {
+    try {
+        const response = await  fetch(url , {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify({
+        //     nbMax: nbMax,
+        //     nbMin: nbMin,
+        // }),
+        });
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+
+    } catch (error) {
+        console.error(error);
+    }
+    };
+
+    useEffect(() => {
+        getAllDecksFromAPI();
+    }, []);
 
     return (
-        <View><Text>Hello World!</Text></View>
+        <View>
+            <View>
+                <Text></Text>
+            </View>
+            <View>
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <FlatList
+                        data={data}
+                        keyExtractor={(item) => item["id_deck"]}
+                        renderItem={({item}) => (
+                            <Pressable>
+                                <Text>{item["titre_deck"]}</Text>
+                            </Pressable>
+                        )}
+                        
+                    /> 
+                )}
+            </View>
+        </View>
+
     );
 }
